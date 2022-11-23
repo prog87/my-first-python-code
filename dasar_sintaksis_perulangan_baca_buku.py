1d4118bbba817a2aa87bc9bddea959c0f4112f6b
prog87 <rizkpython87@gmail.com>
2022-11-24 07:17:51 +0800

    Perintah perulangan dengan for

---
+++ dasar_sintaksis_perulangan_baca_buku.py
@@ -0,0 +1,15 @@
+"""
+Program Perulangan Membaca Buku
+"""
+
+jumlah_buku = 100
+print('Ibu berkata, "Baca semua buku"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+print("Dengan for")
+for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')

065e6c36c893d1608de50a72e8ddb2eac6aa70ef
prog87 <rizkpython87@gmail.com>
2022-11-23 15:53:07 +0800

    Percabangan tingkat lanjut

--- new main.py
+++ new main.py
@@ -24,3 +24,22 @@ else:
 print("Budi pulang ke rumah")
 print("Menyerahkan hasilnya kepada ibu")
 
+# Percabangan tingkat lanjut
+jumlah_susu = 173
+jumlah_butir_telur = 1587
+print(f"jumlah botol susu {jumlah_susu} botol")
+print(f"jumlah telur {jumlah_butir_telur} butir")
+
+if jumlah_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_butir_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyerahkan hasilnya kepada ibu")
+
+

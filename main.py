0746ec858e14061b04933a252402845e7cba1655
Rizky Rahman <rizkpython87@gmail.com>
2023-03-13 21:16:25 +0800

    Sequential

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """
-Ini adalah demo project pertama dengan python
+Semua sintaksis dasar bahasa pemograman terdiri dari :
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("Hello World!")
-print("My name is Kiki")
+
+# Sequential
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Budi menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+

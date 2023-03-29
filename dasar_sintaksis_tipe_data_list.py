4c1e28e5a0fd18b102db130741e8ab946bd3da44
Rizky Rahman <rizkpython87@gmail.com>
2023-03-29 22:01:07 +0800

    mengoperasikan list comprehension part 1

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -60,5 +60,32 @@ for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 # Pop sering digunakan untuk tipe data stack
 
+print("\nPerintah del")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print("\nPerintah del dengan list comprehension")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[:]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print("\nPerintah del dengan list comprehension [start:stop]")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0:3]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nPerintah del dengan list comprehension [start:stop]")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0:-2]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nPerintah del dengan list comprehension [start:stop:step]")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku[0::2]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])

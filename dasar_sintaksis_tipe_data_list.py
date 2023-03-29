74204a6f9b4e84c0b6c5c1d40f56c83ae1d03481
Rizky Rahman <rizkpython87@gmail.com>
2023-03-29 21:37:58 +0800

    mengoperasikan list part 2

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -28,3 +28,37 @@ print('\nTambahkan 1 buku baru')
 daftar_buku.append('Dunia Matematika Kelas 5')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print("\nClear list")
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nGanti elemen pertama")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nAmbil elemen ke-2")
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nBuku yang diambil tadi")
+print(buku)
+
+print("\nPop")
+daftar_buku.pop()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nPop minus")
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftar_buku.pop(-1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+# Pop sering digunakan untuk tipe data stack
+
+
+
